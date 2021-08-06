5770711e5f1529406064a55a068efe751956b91d
burhaneyuboglu <burhaneyuboglu@gmail.com>
2021-08-07 00:27:41 +0100

    Error State

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -7,7 +7,7 @@ import Button from "./Button";
 const ErrorModal = (props) => {
   return (
     <div>
-      <div className={classes.backdrop}/>
+      <div className={classes.backdrop} onClick={props.onSubmit}/>
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -16,7 +16,7 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={classes.action}>
-          <Button>Okay</Button>
+          <Button onClick={props.onSubmit}>Okay</Button>
         </footer>
       </Card>{" "}
     </div>
