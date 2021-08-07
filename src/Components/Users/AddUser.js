d48361866ebf8c381b5c7250935feab8f58dea26
burhaneyuboglu <burhaneyuboglu@gmail.com>
2021-08-07 11:37:22 +0100

    Wrapper

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -3,6 +3,7 @@ import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
@@ -41,7 +42,7 @@ const AddUser = (props) => {
       setError(null);
   }
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onSubmit={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -62,7 +63,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
